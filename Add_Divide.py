da586b8d7fe8bc8e49d16e537409abf2447b7840
jrap-bit <namanpandey10@gmail.com>
2023-02-23 15:44:58 +0530

    Main Calc File and Name Changes

---
+++ Add_Divide.py
@@ -0,0 +1,18 @@
+def add(nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total
+
+
+def divide(a, b):
+    if b == 0:
+        print("Can't divide by zero!")
+<<<<<<< HEAD:Add & Divide.py
+    return a / b
+
+print (add(1, 2, 3, 4, 5))
+print (divide(1, 2))
+=======
+    return a / b
+>>>>>>> 6cc67ce (Main Calc File and Name Changes):Add_Divide.py
