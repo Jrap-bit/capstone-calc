fc0de324aca0de8ed64377d75bba6099ac0d833c
MrDXTR <manav.chillar20@st.niituniversity.in>
2023-02-25 14:23:14 +0530

    Edited Add_Divide

--- Add_Divide.py
+++ Add_Divide.py
@@ -1,18 +1,18 @@
-def add(nums):
+def add(*args):
     total = 0
-    for num in nums:
+    for num in args:
         total += num
     return total
 
-
-def divide(a, b):
-    if b == 0:
-        print("Can't divide by zero!")
-<<<<<<< HEAD:Add & Divide.py
-    return a / b
+def divide(dividend, divisor):
+    try:
+        result = dividend / divisor
+        return result
+    except ZeroDivisionError:
+        print("Error: Division by zero is undefined")
 
 print (add(1, 2, 3, 4, 5))
 print (divide(1, 2))
-=======
-    return a / b
->>>>>>> 6cc67ce (Main Calc File and Name Changes):Add_Divide.py
+
+
+
