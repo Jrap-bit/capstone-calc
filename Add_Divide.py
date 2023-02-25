53d9bddfb4a2a38fa5f4dfe7c9c03865ac3eaf8b
MrDXTR <manav.chillar20@st.niituniversity.in>
2023-02-25 14:27:00 +0530

    Manav's Comit

--- Add_Divide.py
+++ Add_Divide.py
@@ -4,15 +4,10 @@ def add(*args):
         total += num
     return total
 
+
 def divide(dividend, divisor):
     try:
         result = dividend / divisor
         return result
     except ZeroDivisionError:
         print("Error: Division by zero is undefined")
-
-print (add(1, 2, 3, 4, 5))
-print (divide(1, 2))
-
-
-
